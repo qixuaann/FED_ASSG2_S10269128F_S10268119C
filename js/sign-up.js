35382c1a049db9ffbc13af613b92e1676ae57bf9
qixuaann <s10269128@connect.np.edu.sg>
2025-02-04 15:44:10 +0800

    Updated signup page

--- js/sign-up.js
+++ js/sign-up.js
@@ -7,13 +7,13 @@ lottieWrapper.classList.add("lottie-wrapper");
 
 // Create the <dotlottie-player> element
 const lottiePlayer = document.createElement("dotlottie-player");
-lottiePlayer.setAttribute("src", "https://lottie.host/08afce25-529c-475a-9555-70530c541371/fnnULYw7wS.lottie");
+lottiePlayer.setAttribute("src", "https://lottie.host/da183468-6113-4603-baab-41ba7b99a031/hvaZaMbtdi.lottie");
 lottiePlayer.setAttribute("background", "transparent");
 lottiePlayer.setAttribute("speed", "1");
 lottiePlayer.setAttribute("loop", "");
 lottiePlayer.setAttribute("autoplay", "");
-lottiePlayer.style.width = "220%";  // Set width as percentage of container
-lottiePlayer.style.height = "220%"; // Set height as percentage of container
+lottiePlayer.style.width = "90%";  // Set width as percentage of container
+lottiePlayer.style.height = "90%"; // Set height as percentage of container
 
 // Append the Lottie player to the wrapper
 lottieWrapper.appendChild(lottiePlayer);
